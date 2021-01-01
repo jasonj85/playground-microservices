3a1e69fd6b2ca8e8ada04cdd3985baf013bdff97
jasonj85 <jasonj85@gmail.com>
2021-01-01 09:01:05 +0000

    basic skeleton created

---
+++ blog/client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import PostCreate from './PostCreate';
+
+const App = () => {
+  return (
+    <div className="container">
+      <h3>Create Post</h3>
+      <PostCreate />
+    </div>
+  );
+};
+
+export default App;
