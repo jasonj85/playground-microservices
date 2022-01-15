fcf998ee066dbe3c1d645aba16aed8c670773454
jasonj85 <jasonj85@gmail.com>
2022-01-15 10:58:53 +0000

    add very basic demo moderation service

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -19,7 +19,7 @@ app.post("/posts/:id/comments", async (req, res) => {
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: "pending" });
 
   commentsByPostId[req.params.id] = comments;
 
@@ -30,6 +30,7 @@ app.post("/posts/:id/comments", async (req, res) => {
         id: commentId,
         content,
         postId: req.params.id,
+        status: "pending",
       },
     })
     .catch((err) => console.log(err.message));
@@ -37,8 +38,33 @@ app.post("/posts/:id/comments", async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("Event Received", req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((c) => {
+      return c.id === id;
+    });
+
+    comment.status = status;
+
+    await axios
+      .post("http://localhost:4005/events", {
+        type: "CommentUpdated",
+        data: {
+          id,
+          content,
+          postId,
+          status,
+        },
+      })
+      .catch((err) => console.log(err.message));
+  }
 
   res.send({});
 });
--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -5,20 +5,40 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
 
+  events.push(event);
+
+  // posts service
   axios
     .post("http://localhost:4000/events", event)
     .catch((err) => console.log(err.message));
+
+  // comments service
   axios
     .post("http://localhost:4001/events", event)
     .catch((err) => console.log(err.message));
+
+  // query service
   axios
     .post("http://localhost:4002/events", event)
     .catch((err) => console.log(err.message));
 
+  // moderation service
+  axios
+    .post("http://localhost:4003/events", event)
+    .catch((err) => console.log(err.message));
+
+  console.log(req.body);
+
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
