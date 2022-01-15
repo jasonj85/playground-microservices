221cbe98712637fda99f89dc7ba608a61176b3ba
jasonj85 <jasonj85@gmail.com>
2022-01-15 10:07:08 +0000

    adding initial code for microservices

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import PostCreate from './PostCreate';
+import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 
 const App = () => {
   return (
     <div className="container">
       <h3>Create Post</h3>
       <PostCreate />
+      <hr />
+      <h3>Posts</h3>
+      <PostList />
     </div>
   );
 };
---
+++ blog/client/src/CommentList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default ({ comments }) => {
+  const renderedComments = Object.values(comments).map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
